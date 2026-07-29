43490d586644cc5f517821ef552cc579cb312f27
itzy212-lang <itzy212@gmail.com>
2026-07-29 22:54:10 +0100

    Route notice completion to project composer and refresh project

--- src/components/projects/ProjectDetailNoticeWorkflow.jsx
+++ src/components/projects/ProjectDetailNoticeWorkflow.jsx
@@ -1,5 +1,23 @@
+import { useEffect, useState } from 'react';
 import ProjectDetail from './ProjectDetail';
 
 export default function ProjectDetailNoticeWorkflow(props) {
-  return <ProjectDetail {...props} />;
+  const [refreshKey, setRefreshKey] = useState(0);
+
+  useEffect(() => {
+    const openComposer = (event) => {
+      if (event?.detail) props.onOpenComposer?.(event.detail);
+    };
+    const refreshProject = () => setRefreshKey(key => key + 1);
+
+    window.addEventListener('ely:open-project-composer', openComposer);
+    window.addEventListener('ely:refresh-project-detail', refreshProject);
+
+    return () => {
+      window.removeEventListener('ely:open-project-composer', openComposer);
+      window.removeEventListener('ely:refresh-project-detail', refreshProject);
+    };
+  }, [props.onOpenComposer]);
+
+  return <ProjectDetail key={refreshKey} {...props} />;
 }
